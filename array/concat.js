7449d611958ca97967db5427d2825ba87f900cb6
kaycso <kaycsoliveira@hotmail.com>
2024-08-28 12:38:01 -0300

    método concat

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Uasleskah', 'Cibalena']
+const filhos = ['Uesclei', 'Uoxiton']
+
+// O método concat cria um novo array com os elementos passados em seu parâmetro (podendo inclusive passar um outro array)
+const todos = filhas.concat(filhos, 'Fulano')
+console.log(todos)
